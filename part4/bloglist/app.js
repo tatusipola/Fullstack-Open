a0b7cb77e7dc841e7a8524d70649734b35aaeec8
Tatu Sipola <tatusipola@gmail.com>
2022-11-25 12:53:41 +0200

    bloglist 4.1 done, refactored config and mongoose model to own modules

---
+++ part4/bloglist/app.js
@@ -0,0 +1,37 @@
+const http = require('http')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const Blog = require('../models/blog')
+const config = require('./utils/config')
+
+
+mongoose.connect(config.MONGODB_URI)
+
+app.use(cors())
+app.use(express.json())
+
+
+app.get('/api/bloglist', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+app.post('/api/bloglist', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
