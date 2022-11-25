9655a1a23d3f798dcae64f4adb8f0feb5746354c
Tatu Sipola <tatusipola@gmail.com>
2022-11-25 13:24:28 +0200

    refactored routes to own module at /controllers

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -3,8 +3,9 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
-const Blog = require('../models/blog')
+const Blog = require('./models/blog')
 const config = require('./utils/config')
+const bloglistRouter = require('./controllers/bloglist')
 
 
 mongoose.connect(config.MONGODB_URI)
@@ -12,26 +13,9 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors())
 app.use(express.json())
 
+app.use('/api/bloglist', bloglistRouter)
 
-app.get('/api/bloglist', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/bloglist', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = 3003
+const PORT = config.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
