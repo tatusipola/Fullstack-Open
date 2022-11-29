4f4f520aab9d9fae4479f94c5903adc05bdb0e03
Tatu Sipola <tatusipola@gmail.com>
2022-11-29 11:39:49 +0200

    added mostBlogs function and test

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,59 @@ const dummy = (blogs) => {
     return 1
   }
   
-  module.exports = {
-    dummy
-  }
+  
+
+const totalLikes = (blogs) => {
+    const reducer = (sum, item) => {
+        return sum + item.likes
+    }
+
+    return blogs.length === 0
+    ? 0
+    : blogs.reduce(reducer, 0)
+}
+
+const favoriteBlog = (blogs) => {
+    const reducer = (prev, curr) => {
+        return prev.likes > curr.likes ? prev : curr
+    }
+
+    return blogs.length === 0
+    ? 0
+    : blogs.reduce(reducer)
+}
+
+const mostBlogs = (blogs) => {
+    const authors = blogs.map(blog => blog.author)
+
+    if (blogs.length === 0) {
+        return 0
+    }
+
+    //count number of blogs by each author
+    let counter = {}
+    for (element of authors) {
+        if (counter[element]) {
+            counter[element] +=1
+        } else {
+            counter[element] = 1
+        }
+    } 
+
+    const max = Math.max.apply(null, Object.values(counter))
+    for (key in counter) {
+        if (counter[key] === max) {
+            return {
+                [key]: max
+            }
+        }
+    }
+}
+
+
+module.exports = {
+dummy,
+totalLikes,
+favoriteBlog,
+mostBlogs
+}
