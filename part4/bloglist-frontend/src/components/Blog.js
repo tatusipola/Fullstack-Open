d9efb72dc40b3781080b6072cc8b86f93e7af80d
Tatu Sipola <tatusipola@gmail.com>
2022-12-15 22:11:07 +0200

    5.7 added button to show/hide details on blogs

--- part4/bloglist-frontend/src/components/Blog.js
+++ part4/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,41 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  if (showDetails) {
+    return (
+      <div style={blogStyle}>
+        <div>
+          {blog.title} {blog.author} 
+          <button onClick={() => setShowDetails(false)}>hide details</button><br></br>
+          {blog.url} <br></br>
+          likes: {blog.likes} 
+          <button>like</button><br></br>
+          {blog.user ? blog.user.name : ''}
+        </div>
+      </div>
+    )
+  }
+
+  return (
+      <div style={blogStyle}>
+        <div>
+          {blog.title} {blog.author} 
+          <button onClick={() => setShowDetails(true)}>show details</button><br></br>
+        </div>
+      </div>
+    )
+
+
+}
 
 export default Blog
